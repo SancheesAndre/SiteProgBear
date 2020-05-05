4c9b0dd4fa3a3a743a07e585a4576fcb6a58c066
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-05-05 12:54:38 -0300

    [FEAT] navbar leva para locais específicos da página e quando rola o mouse, a marcação é desfeita

--- js/main.js
+++ js/main.js
@@ -1,8 +1,56 @@
 function enviarEmail(email, assunto, corpo) {
     Email.send(
         email,
-        "andreaseusoulin@gmail.com",
+        "teamprogbear@gmail.com",
         assunto,
         corpo
     );
+}
+var btn_pressionado;
+function mudarSelec(classe, pressionado)
+{
+	console.log (classe + "\n" + pressionado);
+	const btns = document.getElementsByClassName(classe);
+	const btn_p = document.getElementById(pressionado);
+	btn_pressionado = btn_p;
+	for (var i = 0; i < btns.length; i++) {
+		if (btns[i] != btn_p)
+		{
+			if (btns[i].classList.contains("Pag-atual"))
+			{
+				btns[i].classList.remove("Pag-atual");
+			}
+			if (!btns[i].classList.contains("Pag-outras"))
+			{
+				btns[i].classList.add("Pag-outras");
+			}
+		}
+		else
+		{
+			if (btns[i].classList.contains("Pag-outras"))
+			{
+				btns[i].classList.remove("Pag-outras");
+			}
+			if (!btns[i].classList.contains("Pag-atual"))
+			{
+				btns[i].classList.add("Pag-atual");
+			}
+		}
+	}
+}
+
+function addOnScroll()
+{
+	console.log('jorge');
+	setTimeout(function()
+	{
+		window.addEventListener('scroll', unselect);
+	}, 200)
+}
+
+function unselect(e)
+{
+	console.log('jorge');
+	btn_pressionado.classList.remove("Pag-atual");
+	window.removeEventListener('scroll', unselect);
 }
